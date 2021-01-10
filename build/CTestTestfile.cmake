6094383a0218a1015e33c2ca51b735b65c597fae
AI_Phile <aiphile17@gmail.com>
2021-01-10 22:36:34 +0500

    adding file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Asadullah/Documents/Projects/CPP_Opencv
+# Build directory: C:/Users/Asadullah/Documents/Projects/CPP_Opencv/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
